75cd02e40f526b12e890ae50841861eedf485111
tirajetw <tirajetw@gmail.com>
2020-05-16 21:40:26 +0700

    version 2

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
 import requests
-import mysql.connector
 import pytz
 from datetime import datetime, timedelta
 import time
+from bs4 import BeautifulSoup
 
 tz = pytz.timezone('Asia/Bangkok')
 refresh_time = "05:00"
-
-mydb = mysql.connector.connect(
-    host="127.0.0.1",
-    user="root",
-    passwd="1234",
-    database="main")
-mycursor = mydb.cursor()
 
 def spacial_round(num):
     num = round(num,3)*10
@@ -26,50 +19,47 @@ def spacial_round(num):
         return result
 
 while True:
-    now = datetime.now(tz)
-    dt_string = now.strftime("%H:%M")
-
-    if dt_string == refresh_time:
-        url = "https://crmmobile.bangchak.co.th/webservice/oil_price.aspx"
-        data = requests.get(url)
-        data.encoding = "utf-8"
-        # print(data.content)
-        from bs4 import BeautifulSoup
-        soup = BeautifulSoup(data.text,'lxml')
-        # print(soup.prettify())
-        x = soup.find_all("type") # <- ค่าที่ใช้ในการค้นหา
-        today_price = soup.find_all("today")
-
-        price_G91 = float(str(today_price[6])[7:12])
-        price_G95 = float(str(today_price[7])[7:12])
-        price_D = float(str(today_price[1])[7:12])
-
-        print('\nToday price')
-        print('G91\t: {} THB/L'.format(price_G91))
-        print('G95\t: {} THB/L'.format(price_G95))
-        print('Diesel\t: {} THB/L'.format(price_D))
-        print('\n')
-
-        sql = "INSERT INTO today_price (gas_id, current_price, sell_price, date) VALUES (%s, %s, %s, %s)"
+    try:
         now = datetime.now(tz)
-        dt_string = now.strftime("%Y-%m-%d")
-        val = [
-            (1, price_G91, spacial_round(price_G91+0.9), dt_string),
-            (2, price_G95, spacial_round(price_G95+0.9), dt_string),
-            (3, price_D, spacial_round(price_D+0.9), dt_string)
-        ]
+        dt_string = now.strftime("%H:%M")
 
-        mycursor.executemany(sql, val)
-        mydb.commit()
+        if dt_string == refresh_time:
+            url = "https://crmmobile.bangchak.co.th/webservice/oil_price.aspx"
+            data = requests.get(url)
+            data.encoding = "utf-8"
+            # print(data.content)
+            soup = BeautifulSoup(data.text,'lxml')
+            # print(soup.prettify())
+            x = soup.find_all("type") # <- ค่าที่ใช้ในการค้นหา
+            today_price = soup.find_all("today")
 
-        mycursor.execute("SELECT * FROM today_price")
-        myresult = mycursor.fetchall()
-        for x in myresult:
-            print(x)
+            price_G91 = (float(str(today_price[6])[7:12]))
+            price_G95 = (float(str(today_price[7])[7:12]))
+            price_D = (float(str(today_price[1])[7:12]))
 
-        print("Wait for check time again.")
-        time.sleep(65)
+            Sprice_G91 = spacial_round(float(str(today_price[6])[7:12])+0.9)
+            Sprice_G95 = spacial_round(float(str(today_price[7])[7:12])+0.9)
+            Sprice_D = spacial_round(float(str(today_price[1])[7:12])+0.9)
 
-    else:
-        print(dt_string)
-        time.sleep(30)
+            print('\nToday price')
+            print('G91\t: {} THB/L'.format(price_G91))
+            print('G95\t: {} THB/L'.format(price_G95))
+            print('Diesel\t: {} THB/L'.format(price_D))
+            print('\n')
+
+            payload = "http://35.240.230.42/production/today_price.php?g91-c=" + str(price_G91) + "&g91-s=" + str(Sprice_G91) + "&g95-c=" + str(price_G95) + "&g95-s=" + str(Sprice_G95) + "&desel-c=" + str(price_D) + "&desel-s=" + str(Sprice_D)
+            # url = "http://35.240.230.42/production/today_price.php?g91-c=0&g91-s=0&g95-c=0&g95-s=0&desel-c=0&desel-s=0"
+            data = requests.get(payload)
+
+            print('send payload to server ,wait for refresh again')
+            time.sleep(65)
+            pass
+
+        else:
+            print(dt_string)
+            time.sleep(30)
+            pass
+
+    except:
+        print('error')
+        pass

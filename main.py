b5cb1005efc5a35a41b784a1faeb81d76ac1ff21
Tirajet Wangngae <tirajetw@JETTs-MacBook-Pro.local>
2020-07-11 23:16:55 +0700

    change to E20

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ while True:
         dt_string = now.strftime("%H:%M")
 
         if dt_string == refresh_time:
+        # if True:
             url = "https://crmmobile.bangchak.co.th/webservice/oil_price.aspx"
             data = requests.get(url)
             data.encoding = "utf-8"
@@ -39,28 +40,28 @@ while True:
             x = soup.find_all("type") # <- ค่าที่ใช้ในการค้นหา
             today_price = soup.find_all("today")
 
-            price_G91 = (float(str(today_price[6])[7:12]))
+            price_E20 = (float(str(today_price[5])[7:12]))
             price_G95 = (float(str(today_price[7])[7:12]))
             price_D = (float(str(today_price[1])[7:12]))
 
-            Sprice_G91 = spacial_round(float(str(today_price[6])[7:12])+0.9)
+            Sprice_E20 = spacial_round(float(str(today_price[5])[7:12])+0.9)
             Sprice_G95 = spacial_round(float(str(today_price[7])[7:12])+0.9)
             Sprice_D = spacial_round(float(str(today_price[1])[7:12])+0.9)
 
             print('\nToday price')
-            print('G91\t: {} THB/L'.format(price_G91))
+            print('E20\t: {} THB/L'.format(price_E20))
             print('G95\t: {} THB/L'.format(price_G95))
             print('Diesel\t: {} THB/L'.format(price_D))
             print('\n')
 
             print('\nSell price')
-            print('G91\t: {} THB/L'.format(Sprice_G91))
+            print('E20\t: {} THB/L'.format(Sprice_E20))
             print('G95\t: {} THB/L'.format(Sprice_G95))
             print('Diesel\t: {} THB/L'.format(Sprice_D))
             print('\n')
 
-            payload = "https://www.banthagroup.com/production/today_price.php?g91-c=" + str(price_G91) + "&g91-s=" + str(Sprice_G91) + "&g95-c=" + str(price_G95) + "&g95-s=" + str(Sprice_G95) + "&desel-c=" + str(price_D) + "&desel-s=" + str(Sprice_D)
-            # url = "https://www.banthagroup.com/production/today_price.php?g91-c=0&g91-s=0&g95-c=0&g95-s=0&desel-c=0&desel-s=0"
+            payload = "https://www.banthagroup.com/production/today_price.php?g91-c=" + str(price_E20) + "&g91-s=" + str(Sprice_E20) + "&g95-c=" + str(price_G95) + "&g95-s=" + str(Sprice_G95) + "&desel-c=" + str(price_D) + "&desel-s=" + str(Sprice_D)
+            # url = "https://www.banthagroup.com/production/today_price.php?E20-c=0&E20-s=0&g95-c=0&g95-s=0&desel-c=0&desel-s=0"
             data = requests.get(payload)
 
             print('send payload to server ,wait for refresh again')
